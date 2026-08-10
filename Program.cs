a90cc6aadf81f2dabc61bf65ede55b6b3bb6ce13
TomasRosciolino <tomasrosciolino@gmail.com>
2026-08-10 09:40:40 -0300

    etapa 1: bienvenida y datos del cajero

--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string NombreComercio = "lettera";
+Console.WriteLine($"{NombreComercio}");
+Console.Write("Ingrese el nombre de la persona que atiende en la caja: ");
+string nombre = Console.ReadLine();
+Console.WriteLine($"el nombre del cajero es {nombre}");
+Console.WriteLine($"Bienvenido/a {nombre}. Caja abierta");
+Console.ReadKey();
